b708ab482030565b21708ff0df88d172fdfac524
Leeyubin <taget17@naver.com>
2017-05-12 11:11:22 +0900

    nb

--- JavaPractice/src/org/dimigo/inheritance/PersonTest.java
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest.java
@@ -20,25 +20,23 @@ public class PersonTest {
 	    * @param args
 	    */
 	   public static void main(String[] args) {
-	      Person t=new Person("Tom");
-	      Korean h=new Korean("홍길동");
-	      Japanese d =new Japanese("다나카");
-	      Chinese w=new Chinese("왕밍");
+	      Person[] p = {
+	    		new Person("Tom"),
+	    		new Korean("홍길동"),
+	    		new Japanese("다나카"),
+	    		new Chinese("왕밍")
+	      };
 	      
-	      System.out.println(t);
-	      System.out.println(h);
-	      System.out.println(d);
-	      System.out.println(w);
-	      System.out.println();
-	      t.sayHello();
-	      h.sayHello();
-	      d.sayHello();
-	      w.sayHello();
-	      System.out.println();
-	      t.sayBye();
-	      h.sayBye();
-	      d.sayBye();
-	      w.sayBye();
+	      for (Person person : p){
+	    	  dlstk(person);
+	      }
+	   }
+	   
+	   private static void dlstk(Person p){
+		   System.out.println(p);
+		   p.sayHello();
+		   p.sayBye();
+	     System.out.println();
 	   }
 
 	}
